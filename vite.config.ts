84a918e17c559be07d61352924aa99113875121e
Sergey Gromov <sendtogromov@gmail.com>
2021-10-03 14:53:44 +0300

    [+] vite-plugin-inspect

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { ViteAliases } from "vite-aliases";
 import { qrcode } from 'vite-plugin-qrcode';
+import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
-  plugins: [reactRefresh(), ViteAliases(), qrcode()],
+  plugins: [reactRefresh(), ViteAliases(), qrcode(), Inspect()],
 });
